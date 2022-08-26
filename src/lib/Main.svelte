1cbdeac7cc2d615b5fa5b28883d57b1a608db0c1
kushagra-oncorp <kushagra@onecorp.co.in>
2022-08-26 17:58:29 +0530

    added animation to hero-icon

--- src/lib/Main.svelte
+++ src/lib/Main.svelte
@@ -1,7 +1,8 @@
 <script>
-
+import HeroIcon from './../assets/home-bg.png';
 </script>
 
 <div class=''>
     <h1>Welcome To <span>Better<span>T</span>ube</span></h1>
+    <img src={HeroIcon} alt="" class="hero-icon">
 </div>
