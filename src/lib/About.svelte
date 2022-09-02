0087385b9ee3e830e171a65d3be1f4ffc9bf2373
kushagra-oncorp <kushagra@onecorp.co.in>
2022-09-02 10:43:12 +0530

    minor UI Fix

--- src/lib/About.svelte
+++ src/lib/About.svelte
@@ -1,7 +1,7 @@
 <script></script>
 <section class="about-container">
     <h2>Nobody does it better</h2>
-    <p>We are the best video straming service you can ever findout in the wild.
+    <p>We are the best video straming service you can ever find out in the wild.
         <span>
             <i> &#9733;</i>
             <i> &#9733;</i>
