3024a1b766b0a0126b2cb5fb54f7ad4c0ab480f0
kushagra-oncorp <kushagra@onecorp.co.in>
2022-09-06 10:28:38 +0530

    added fatures

--- src/lib/Features.svelte
+++ src/lib/Features.svelte
@@ -1,10 +1,17 @@
 <script>
-
 </script>
 
-<section class="features-containers">
-<h3>our best features</h3>
-<p>
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam reiciendis autem temporibus, fugiat dolore quasi quam impedit at suscipit enim accusamus eaque quod nostrum porro quis! Fugiat consectetur velit provident quas, pariatur nulla mollitia repellat blanditiis explicabo distinctio accusamus sed, facere doloremque eligendi suscipit recusandae aliquam fuga iure reiciendis atque.
-</p>
+<section class="features-container">
+  <h3>our best features</h3>
+  <p>
+    BetteTube is a web video player built from the ground up by SvelteJS. It
+    supports modern streaming formats, as well as YouTube, Vimeo, and even Flash
+    (through plugins, more on that later).
+  </p>
+
+  <p>
+    It supports video playback on desktop and mobile devices. The project was
+    started mid 2022, and now has hundreds of contributors and is used on over
+    45,000 websites.
+  </p>
 </section>
